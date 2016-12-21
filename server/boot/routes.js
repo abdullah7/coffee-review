388ff2d54fe9af0bad68595ddaa4aadcd5f624fa
abdullah7 <fastvirus777@gmail.com>
2016-12-21 15:21:55 +0500

    Add custom express routes

---
+++ server/boot/routes.js
@@ -0,0 +1,8 @@
+module.exports = function (app) {
+    var router = app.loopback.Router();
+    router.get('/ping', function (req, res) {
+        res.send('pong');
+    });
+
+    app.use(router);
+}
